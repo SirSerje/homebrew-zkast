1afb58de0c2db3ae46275201803b1a00343bb9cf
Serhii Viazkov <serhii.viazkov@amcsgroup.com>
2025-12-05 13:08:49 -0500

    Install package and deps separately to avoid dylib issue

--- zkast.rb
+++ zkast.rb
@@ -1,6 +1,4 @@
 class Zkast < Formula
-  include Language::Python::Virtualenv
-
   desc "Command-line tool for managing your notes with a beautiful TUI interface"
   homepage "https://github.com/SirSerje/zkast"
   url "https://github.com/SirSerje/zkast/archive/refs/tags/v0.1.0.tar.gz"
@@ -10,18 +8,10 @@ class Zkast < Formula
   depends_on "python3"
 
   def install
-    virtualenv_install_with_resources
-  end
-
-  def post_install
-    # Prevent Homebrew from trying to fix dylib IDs for pydantic_core
-    # The binary header is too small, but the package works fine without fixing
-    python_version = "3.14"
-    pydantic_core_path = libexec/"lib/python#{python_version}/site-packages/pydantic_core"
-    if pydantic_core_path.exist?
-      # Create a marker file to indicate this was handled
-      (pydantic_core_path/".homebrew_skip_relocation").write("")
-    end
+    # Use pip install with --no-deps first, then install dependencies
+    # This prevents Homebrew from trying to fix dylib IDs on the main package
+    system "python3", "-m", "pip", "install", "--no-deps", "--prefix=#{prefix}", "--no-warn-script-location", "."
+    system "python3", "-m", "pip", "install", "--prefix=#{prefix}", "--no-warn-script-location", "-r", buildpath/"requirements.txt"
   end
 
   test do
