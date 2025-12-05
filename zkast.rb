3c36d20b50b864ab5521ab843991bb2e2142cfcd
Serhii Viazkov <serhii.viazkov@amcsgroup.com>
2025-12-05 12:53:30 -0500

    Add zkast formula v0.1.0

--- zkast.rb
+++ zkast.rb
@@ -2,16 +2,17 @@ class Zkast < Formula
   desc "A command-line tool for managing your notes with a beautiful TUI interface"
   homepage "https://github.com/SirSerje/zkast"
   url "https://github.com/SirSerje/zkast/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7a75c6e0dd693969712369f99996620c8018097d18d98138d1a5a67db6cb9912" # Run: shasum -a 256 <tarball> after creating release
+  sha256 "7a75c6e0dd693969712369f99996620c8018097d18d98138d1a5a67db6cb9912"
   license "MIT"
 
   depends_on "python3"
 
   def install
-    system "python3", "-m", "pip", "install", "--prefix=#{prefix}", "."
+    system "python3", "-m", "pip", "install", "--prefix=#{prefix}", "--no-warn-script-location", "."
   end
 
   test do
-    system "#{bin}/zkast", "--version"
+    assert_match "version 0.1.0", shell_output("#{bin}/zkast --version")
+    system "#{bin}/zkast", "--help"
   end
 end
