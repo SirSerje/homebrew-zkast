578b2d8d4aaa2e6e69a2e8c4494bcf9b62df5e78
Serhii Viazkov <serhii.viazkov@amcsgroup.com>
2025-12-05 12:53:54 -0500

    Fix formula style issues

--- zkast.rb
+++ zkast.rb
@@ -1,5 +1,5 @@
 class Zkast < Formula
-  desc "A command-line tool for managing your notes with a beautiful TUI interface"
+  desc "Command-line tool for managing your notes with a beautiful TUI interface"
   homepage "https://github.com/SirSerje/zkast"
   url "https://github.com/SirSerje/zkast/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "7a75c6e0dd693969712369f99996620c8018097d18d98138d1a5a67db6cb9912"
@@ -13,6 +13,6 @@ class Zkast < Formula
 
   test do
     assert_match "version 0.1.0", shell_output("#{bin}/zkast --version")
-    system "#{bin}/zkast", "--help"
+    system bin/"zkast", "--help"
   end
 end
